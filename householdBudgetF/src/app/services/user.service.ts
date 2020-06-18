41bb9393e71c43ba552d83fe0de092051f358add
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2020-06-18 19:41:24 +0200

    user service added

---
+++ householdBudgetF/src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Cashflow} from "../model/Cashflow";
+import {User} from "../model/User";
+
+export class AuthService {
+
+  baseurl = "http://localhost:8080/api";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getAllUsersForHousehold(): Observable<User> {
+    return this.httpClient.get<User>(this.baseurl + "/user/household");
+  }
+
+}
