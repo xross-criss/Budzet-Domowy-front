165e8c736dcbfcf6231c0edc9751c7edad8f791b
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2020-08-14 14:53:06 +0200

    home page implemented

--- src/app/ui/home/home/home.component.ts
+++ src/app/ui/home/home/home.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {Balance} from '../../../model/Balance';
+import {User} from '../../../model/User';
+import {BalanceController} from '../../../controllers/balance.controller';
+import {UserController} from '../../../controllers/user.controller';
+import {BalanceType} from '../../../model/dictionary/BalanceType';
 
 @Component({
     selector:    'app-home',
@@ -7,9 +12,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    constructor() { }
+    public summary: Balance;
+    public users: User[];
+    public username: string = localStorage.getItem('token').split(':')[0];
+    public loggedInUser: User;
 
-    ngOnInit(): void {
+    constructor(private balanceController: BalanceController, private userController: UserController) {
     }
 
+    ngOnInit(): void {
+        this.balanceController.getBalances().subscribe(balance => {
+            this.summary = balance.find((value: Balance) => value.type === BalanceType.SUMMARY);
+        });
+        this.userController.getAllUsersForHousehold().subscribe(users => {
+            this.users = users;
+        });
+    }
 }
