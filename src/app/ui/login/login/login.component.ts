d1ea0cdae8ac29b4fd960a4653ae915128af31ab
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2020-08-14 14:31:56 +0200

    login module implemented

--- src/app/ui/login/login/login.component.ts
+++ src/app/ui/login/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthController} from '../../../controllers/auth.controller';
+import {delay} from 'rxjs/operators';
 
 @Component({
     selector: 'app-login',
@@ -10,33 +11,37 @@ import {AuthController} from '../../../controllers/auth.controller';
 })
 export class LoginComponent implements OnInit {
 
-    loginForm = new FormGroup({
-        username: new FormControl('', [Validators.required]),
-        password: new FormControl('', [Validators.required])
-    });
+    public loginForm: FormGroup;
 
-    loading = false;
-    error = false;
+    public loading = false;
+    public error = false;
 
     constructor(private authController: AuthController, private router: Router) {
     }
 
-    public login(): void {
-        this.loading = true;
-        this.error = false;
-        this.authController.authorize(this.loginForm.value.username, this.loginForm.value.password)
-            .then(token => {
-                this.loading = false;
-                localStorage.setItem('token', token.token);
-                this.router.navigate(['/']);
-            })
-            .catch(err => {
-                this.loading = false;
-                this.error = true;
-            });
+    public ngOnInit(): void {
+        this.loginForm = new FormGroup({
+            username: new FormControl('', [Validators.required]),
+            password: new FormControl('', [Validators.required])
+        });
     }
 
-    ngOnInit(): void {
+    public login(): void {
+        if (this.loginForm.invalid) {
+            return;
+        }
+        this.loading = true;
+        this.error = false;
+        this.authController.authorize(this.loginForm.value.username, this.loginForm.value.password).pipe(
+            delay(3000),
+        ).subscribe(token => {
+            this.loading = false;
+            localStorage.setItem('token', token.token);
+            this.router.navigate(['/']);
+        }, err => {
+            this.loading = false;
+            this.error = true;
+        });
     }
 
 }
